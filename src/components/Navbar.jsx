060ef34a33ba03dd04fe9997abfb9552ce92b433
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-06-05 22:15:28 +1000

    init repo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import phoneImg from "../assets/phone.png";
+import userImg from "../assets/user.png";
+import downArrowImg from "../assets/down-arrow.png";
+
+export default function Navbar() {
+    return (
+        <div className="nav">
+            <span className="nav--title">find your contacts</span>
+            <img className="nav--logo" src={phoneImg}></img>
+            <div className="nav--user">
+                <img className="nav--user--pfp" src={userImg}></img>
+                <span className="nav--user--name">Luong N.</span>
+                <img className="nav--user--arrow" src={downArrowImg}></img>
+            </div>
+        </div>
+    )
+}
