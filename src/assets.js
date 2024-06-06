1935d3bceb565aca58a4f77126e48d4282cd34ef
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-06-06 16:23:31 +1000

    import icons from 1 js file

---
+++ src/assets.js
@@ -0,0 +1,15 @@
+import phoneImg from "./assets/icons/phone.png"
+import userImg from "./assets/icons/user.png"
+import downArrowImg from "./assets/icons/down-arrow.png"
+import searchImg from './assets/icons/search.png'
+import linkedinImg from "./assets/icons/linkedin.png"
+import twitterImg from "./assets/icons/twitter.png"
+
+export default {
+    phoneImg,
+    userImg,
+    downArrowImg,
+    searchImg,
+    linkedinImg,
+    twitterImg
+}
