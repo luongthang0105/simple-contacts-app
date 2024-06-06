c8b578ff6a5d4be0c453ce6813ecf5c416d0bd73
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-06-06 16:25:11 +1000

    wrap img, import pfps

--- src/components/Content/ContactCards.jsx
+++ src/components/Content/ContactCards.jsx
@@ -1,29 +1,37 @@
-import pfp1 from "../../assets/profile_picture (1).jpg"
-import linkedinImg from "../../assets/icons/linkedin.png"
-import twitterImg from "../../assets/icons/twitter.png"
+import icons from "../../assets"
 import data from "../../../data/data.json";
 import PropTypes from 'prop-types';
-
 // const profilePictures = require.context('../')
 
 export default function ContactCards(props) {
     const inputText = props.inputText
     const filteredUsers = data.filter((user) => {
         const userIdentifier = (user.name + '|' + user.email + '|' + user.website + '|' + user.phone).toLowerCase()
-        return  userIdentifier.includes(inputText)
+        return userIdentifier.includes(inputText)
     })
+    
+    let idToPfp = {}
+    filteredUsers.forEach((user) => {
+        idToPfp[user.id] = `src/assets/pfp/pfp(${user.id}).png`
+    })
+
     return (
         filteredUsers.map((user) => (
             <div className="content--contacts--card" key={user.id}>
-                <img className="content--contacts--card--img" src={pfp1}></img>
-                <span className="content--contacts--card--name">{user.name}</span>
-                <span className="content--contacts--card--email">{user.email}</span>
-                <span className="content--contacts--card--website">{user.website}</span>
-                <span className="content--contacts--card--phone">{user.phone}</span>
-                <div className="content--contacts--card--socials">
-                    <img className="contacts--card--socials--media" src={linkedinImg}></img>
-                    <img className="contacts--card--socials--media" src={twitterImg}></img>
+                <div className="contacts--card--img-wrapper">
+                    <img className="content--contacts--card--img" src={require(idToPfp[user.id])}></img>
                 </div>
+                <div className="content--contacts--card--info">
+                    <span className="content--contacts--card--name">{user.name}</span>
+                    <span className="content--contacts--card--email">{user.email}</span>
+                    <span className="content--contacts--card--website">{user.website}</span>
+                    <span className="content--contacts--card--phone">{user.phone}</span>
+                    <div className="content--contacts--card--socials">
+                        <img className="contacts--card--socials--media" src={icons.linkedinImg}></img>
+                        <img className="contacts--card--socials--media" src={icons.twitterImg}></img>
+                    </div>
+                </div>
+                
             </div>      
         ))
     )
